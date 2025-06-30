e68d233a1630c501328e8a3d56b6d19cb9653c7b
Jape <143193834+jpmartirez@users.noreply.github.com>
2025-06-30 17:37:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: 'postgres',
   host: 'localhost',
-  database: 'permalist',
-  password: 'johnpaul',
+  database: '******',
+  password: '******',
   port: 5432
 });
 
